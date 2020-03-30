cb07beacf0470fbfb5356782dfb46a23e157baa0
Thiago Yudji <thiyudji@gmail.com>
2020-03-30 17:00:10 -0300

    First commit

---
+++ src/test/java/br/com/smu/springcrudex/SpringCrudExApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.smu.springcrudex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
